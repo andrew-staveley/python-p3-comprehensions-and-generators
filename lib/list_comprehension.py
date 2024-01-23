377bd1943ab2bdbde0c10be3d50caa370dc936c2
Andrew <andrewstaveley14@gmail.com>
2024-01-22 21:00:51 -0500

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_num_list = [n for n in num_list if (n % 2) == 0]
+    return even_num_list
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = []
+    for sentence in sentence_list:
+        new_list.append(sentence + "!")
+    return new_list
